a1fed3362406ea8f00540f26ebc0929a29c24ac6
wolray <290061869@qq.com>
2016-02-16 02:02:00 +0800

    (+ex2 +scipy) -sum (?return ?,)

--- ex1.py
+++ ex1.py
@@ -1,28 +1,30 @@
 from numpy import *
 
+def h0(X,theta):
+    return X.dot(theta)
+
 def computeCost(X,y,theta):
-    m=size(y,0)
-    J=1/(2*m)*sum((X*theta-y).A**2)
+    m=y.shape[0]
+    J=1/(2*m)*sum((h0(X,theta)-y)**2)
     return J
 
 def gradientDescent(X,y,theta,alpha,num_iters):
-    m=size(y,0)
+    m=y.shape[0]
     J_history=zeros((num_iters,1))
     for i in range(num_iters):
-        theta=theta-alpha/m*X.T*(X*theta-y)
+        theta=theta-alpha/m*X.T.dot(h0(X,theta)-y)
         J_history[i]=computeCost(X,y,theta)
-        return [theta,J_history]
+        return theta,J_history
 
-data=mat(loadtxt('ex1data1.txt',delimiter=','))
-X=data[:,0]
-y=data[:,1]
-m=size(y,0)
-X=c_[ones((m,1)),X]
-theta=zeros((2,1))
+data=loadtxt('ex1data1.txt',delimiter=',')
+m,n=data.shape
+X=c_[ones((m,1)),data[:,:n-1]]
+y=data[:,n-1:]
+init_theta=zeros((n,1))
 
 iterations=1500
 alpha=0.01
 
-J=computeCost(X,y,theta)
-theta=gradientDescent(X,y,theta,alpha,iterations)[0]
+J=computeCost(X,y,init_theta)
+theta=gradientDescent(X,y,init_theta,alpha,iterations)[0]
 print(J)
